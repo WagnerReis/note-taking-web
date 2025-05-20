4e1230eec701c0beac99ab0290e3ad3fa3b15e3a
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-20 14:51:29 -0300

    feat(theme): add dark mode support with next-themes

---
+++ src/providers/custom-theme-provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { ReactNode } from "react";
+
+interface CustomThemeProviderProps {
+  children: ReactNode;
+}
+
+export function CustomThemeProvider({ children }: CustomThemeProviderProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      storageKey="@note-taking-theme"
+      themes={["dark", "light"]}
+      enableSystem
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
