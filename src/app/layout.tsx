ef4a39a38f898164d56a45e5626a4f86ea4ee2e1
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-21 09:23:43 -0300

    refactor(styles): update dark mode implementation and remove Geist fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CustomThemeProvider } from "@/providers/custom-theme-provider";
 import { inter, notoSerif, sourceCodePro } from "./fonts";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,12 +16,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      suppressHydrationWarning
       className={`${sourceCodePro.variable} ${notoSerif.variable} ${inter.variable}`}
+      suppressHydrationWarning
     >
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <CustomThemeProvider>{children}</CustomThemeProvider>
       </body>
     </html>
