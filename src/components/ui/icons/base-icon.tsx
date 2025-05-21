9bf16e28dac529a0517774f79e114421a10653ca
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-21 09:24:21 -0300

    feat(ui): add BaseIcon component and IconProps type

---
+++ src/components/ui/icons/base-icon.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import clsx from "clsx";
+import { useEffect, useState, ReactNode } from "react";
+
+type BaseIconProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+export default function BaseIcon({ children, className }: BaseIconProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  console.log(className);
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox={"0 0 24 24"}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      className={clsx("text-neutral-900 dark:text-neutral-300", className)}
+    >
+      {children}
+    </svg>
+  );
+}
