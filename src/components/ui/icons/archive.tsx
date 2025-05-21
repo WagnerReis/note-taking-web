b94b0827efdb3134c9c08ca04e493d7ee318deed
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-21 09:24:55 -0300

    feat(icons): add Archive and ArrowLeft2 components

---
+++ src/components/ui/icons/archive.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { IconProps } from "@/types";
+import BaseIcon from "./base-icon";
+
+export function Archive(props: IconProps) {
+  return (
+    <BaseIcon {...props}>
+      <path
+        d="M21 7.78216V16.2169C21 19.165 18.9188 21 15.9736 21H8.02638C5.08119 21 3 19.165 3 16.2159V7.78216C3 4.83405 5.08119 3 8.02638 3H15.9736C18.9188 3 21 4.84281 21 7.78216Z"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15 14L11.9982 17L9 14"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.998 17V10"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.9336 7H3.05859"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </BaseIcon>
+  );
+}
---
+++ src/components/ui/icons/arrow-left2.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { IconProps } from "@/types";
+import BaseIcon from "./base-icon";
+
+export function ArrowLeft2(props: IconProps) {
+  return (
+    <BaseIcon {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M15.7499 20.414L7.33594 12L15.7499 3.586L17.1639 5L10.1639 12L17.1639 19L15.7499 20.414Z"
+        fill="currentColor"
+      />
+    </BaseIcon>
+  );
+}
