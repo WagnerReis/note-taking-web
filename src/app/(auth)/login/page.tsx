e5dfa361802d9d458cab594a8c739e305f4b8a01
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-23 06:49:13 -0300

    refactor(auth): split login page into server and client components

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,121 +1,14 @@
-"use client";
-import { Preset1, Preset3, Preset5 } from "@/components/typography";
-import { Button } from "@/components/ui/button";
-import { Divider } from "@/components/ui/divider";
-import { Logo } from "@/components/ui/icons/logo";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { GoogleLogo } from "phosphor-react";
-import { useForm } from "react-hook-form";
-import { twMerge } from "tailwind-merge";
-import { z } from "zod";
+import { cookies } from "next/headers";
+import LoginPage from "./LoginPage";
+import { redirect } from "next/navigation";
 
-const loginFormSchema = z.object({
-  email: z.string().email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
-});
+export default async function Page() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("authToken")?.value;
 
-type LoginFormDataType = z.infer<typeof loginFormSchema>;
-
-export default function Login() {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<LoginFormDataType>({
-    resolver: zodResolver(loginFormSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const router = useRouter();
-
-  const emailError = errors.email?.message;
-  const passwordError = errors.password?.message;
-
-  async function onSubmit(data: LoginFormDataType) {
-    await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-      credentials: "include",
-    });
-
-    reset();
-    router.push("/");
+  if (token) {
+    redirect("/");
   }
 
-  return (
-    <main
-      className={twMerge(
-        "flex w-[343px] flex-col bg-white md:w-[540px] dark:bg-neutral-950",
-        "shadow-small border border-neutral-200 px-200 dark:border-neutral-800",
-        "rounded-16 items-center gap-4 py-600 md:px-600",
-      )}
-    >
-      <Logo width={95} height={28} />
-
-      <section className="mt-2">
-        <Preset1>Welcome to note</Preset1>
-        <Preset5 className="mt-2 text-center text-neutral-600 dark:text-neutral-300">
-          Please log in to continue
-        </Preset5>
-      </section>
-
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex w-full flex-col gap-4"
-      >
-        <Input
-          label="Email Address"
-          type="email"
-          placeholder="email@example.com"
-          error={emailError}
-          {...register("email")}
-        />
-
-        <Input
-          label="Password"
-          type="password"
-          showPasswordToggle
-          error={passwordError}
-          {...register("password")}
-        />
-
-        <Button intent="primary" text="Login" type="submit" />
-      </form>
-
-      <Divider className="mt-1" />
-      <section className="w-full space-y-4">
-        <Preset5 className="text-center text-neutral-300 dark:text-neutral-600">
-          Or log in with:
-        </Preset5>
-
-        <button
-          className={twMerge(
-            "rounded-12 flex h-[43px] w-full cursor-pointer",
-            "items-center justify-center gap-3 border border-neutral-300 dark:border-neutral-600",
-            "hover:bg-neutral-50 dark:hover:bg-neutral-900",
-          )}
-        >
-          <GoogleLogo size={20} weight="bold" />
-          <Preset3 className="text-neutral-950 dark:text-white">Google</Preset3>
-        </button>
-      </section>
-      <Divider />
-      <Preset5 className="mt-2 text-neutral-300 dark:text-neutral-600">
-        No account yet?{" "}
-        <Link href="/register" className="text-neutral-950 dark:text-white">
-          Sign Up
-        </Link>
-      </Preset5>
-    </main>
-  );
+  return <LoginPage />;
 }
